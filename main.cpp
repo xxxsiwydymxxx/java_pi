93e7522d75205dc0034f12e0410fb8e83a8fdf7a
danielwiszowaty <51140724+danielwiszowaty@users.noreply.github.com>
2019-12-08 13:00:42 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+enum class Day{
+    pn=1, wt=2, sr=3, czw=4, pt=5, sb=6, nd=7
+};
+
+struct Prowadzacy{
+    string Godzina;
+    Day Dzien;
+    string Grupa;
+    string Nazwisko;
+    string Przedmiot;
+    Prowadzacy* pNext;
+};
+
+void dodajProwadzacego(Prowadzacy*& pHead, string Godzina, Day Dzien, string Grupa, string Nazwisko, string Przedmiot)
+{
+    //gdy lista jest pusta
+    if (pHead == nullptr)
+    {
+        pHead = new Prowadzacy{Godzina, Dzien, Grupa, Nazwisko, Przedmiot, nullptr};
+    }
+    else
+    {
+        //sortowanie wg. godziny i daty
+        Prowadzacy* temp = pHead;
+        if (true)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
+
+
+
+
+void usunListeIteracyjnie(Prowadzacy*& pHead)
+{
+    while (pHead)
+    {
+        auto p = pHead;
+        pHead = pHead->pNext;
+        delete p;
+    }
+}
+
+int main()
+{
+    return 0;
+}
