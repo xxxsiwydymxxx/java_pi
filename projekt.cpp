f826f888f8ea6bb8759cc3074c76bcfe0ae6ed00
danielwiszowaty <51140724+danielwiszowaty@users.noreply.github.com>
2019-12-08 13:41:47 +0100

    Update projekt.cpp

--- projekt.cpp
+++ projekt.cpp
@@ -5,19 +5,21 @@ using namespace std;
 enum class Day{
     pn=1, wt=2, sr=3, czw=4, pt=5, sb=6, nd=7
 };
-
-struct Zajecia{
-    string Godzina;
-    Day Dzien;
-    string Grupa;
-    string Przedmiot;  
-};
-
 struct Prowadzacy{
     string Nazwisko;
-    Zajecia* pNext;
+    Prowadzacy* pNext;
 };
 
+struct Zajecia{
+    string GodzinaPocz;
+    string GodzinaKon;
+    Day Dzien;
+    string Grupa;
+    string Przedmiot;
+    Prowadzacy* pZajecia;
+    Zajecia* pNext;
+    
+};
 
 int main()
 {
